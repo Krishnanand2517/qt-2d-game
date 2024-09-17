8db79776901abe2fee0fe6792c9cfd71b830299b
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-09-17 12:36:45 +0530

    feat: Add the first rectangle to a scene

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // create a scene
+    QGraphicsScene *scene = new QGraphicsScene();
+
+    // create an item
+    QGraphicsRectItem *rect = new QGraphicsRectItem();
+    rect->setRect(0, 0, 100, 100);
+
+    // add it into the scene
+    scene->addItem(rect);
+
+    // create a view and show it
+    QGraphicsView *view = new QGraphicsView(scene);
+    view->show();
 
     return a.exec();
 }
