2901d29142e7a9a360a0c0c5c7d0d7acd0f6f44b
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-09-17 15:01:13 +0530

    feat: Move the rectangle with keyboard input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <QApplication>
 #include <QGraphicsScene>
-#include <QGraphicsRectItem>
 #include <QGraphicsView>
+
+#include "MyRect.h"
+
 
 int main(int argc, char *argv[])
 {
@@ -11,11 +13,15 @@ int main(int argc, char *argv[])
     QGraphicsScene *scene = new QGraphicsScene();
 
     // create an item
-    QGraphicsRectItem *rect = new QGraphicsRectItem();
+    MyRect *rect = new MyRect();
     rect->setRect(0, 0, 100, 100);
 
     // add it into the scene
     scene->addItem(rect);
+
+    // make rect focusable and focus on it (to receive input events)
+    rect->setFlag(QGraphicsItem::ItemIsFocusable);
+    rect->setFocus();
 
     // create a view and show it
     QGraphicsView *view = new QGraphicsView(scene);
